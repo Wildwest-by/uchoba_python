7e0eed323ed47db0b65ba4fe023b687e7dffd639
Pavel <Stelazh@tut.by>
2022-10-31 22:44:44 +0300

    Закончил 7 задание.

--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -21,4 +21,9 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+word_1 = secret_message[0][3]
+word_2 = secret_message[1][9:13]
+word_3 = secret_message[2][5:15:2]
+word_4 = secret_message[3][12:6:-1]
+word_5 = secret_message[4][20:15:-1]
+print(word_1, word_2, word_3, word_4, word_5)
